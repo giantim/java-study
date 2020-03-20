8925aa4f876805511a7dcf7e266ab9ed1a901e26
giantim <giantim132@gmail.com>
2020-03-20 17:48:50 +0900

    feat: 계산기능의 공통 부분을 인터페이스로 추출해서 구현

--- src/main/java/techcourse/fp/mission/Calculator.java
+++ src/main/java/techcourse/fp/mission/Calculator.java
@@ -3,33 +3,42 @@ package techcourse.fp.mission;
 import java.util.List;
 
 public class Calculator {
-    public static int sumAll(List<Integer> numbers) {
+    public static int sumAll(List<Integer> numbers, Conditional conditional) {
         int total = 0;
         for (int number : numbers) {
-            total += number;
-        }
-        return total;
-    }
-
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
+            if (conditional.test(number)) {
                 total += number;
             }
         }
         return total;
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
+    public static int sumAllEven(List<Integer> numbers, Conditional conditional) {
+        int total = 0;
+        for (int number : numbers) {
+            if (conditional.test(number)) {
+                total += number;
+            }
+        }
+        return total;
+    }
+
+    public static int sumAllOverThree(List<Integer> numbers, Conditional conditional) {
         int total = 0;
 
         //TODO: List에 담긴 값 중 3보다 큰 수만을 더해야 한다.
-        total = numbers.stream()
-                .filter(number -> number > 3)
+        for (int number : numbers) {
+            if (conditional.test(number)) {
+                total = total + number;
+            }
+        }
+        return total;
+    }
+
+    public static int sumByConditional(List<Integer> numbers, Conditional conditional) {
+        return numbers.stream()
+                .filter(conditional::test)
                 .mapToInt(Integer::intValue)
                 .sum();
-
-        return total;
     }
 }
--- src/test/java/techcourse/fp/mission/CalculatorTest.java
+++ src/test/java/techcourse/fp/mission/CalculatorTest.java
@@ -12,19 +12,30 @@ class CalculatorTest {
 
     @Test
     public void sumAll() {
-        int sum = Calculator.sumAll(numbers);
+        int sum = Calculator.sumAll(numbers, (number) -> true);
         assertThat(sum).isEqualTo(21);
     }
 
     @Test
     public void sumAllEven() {
-        int sum = Calculator.sumAllEven(numbers);
+        int sum = Calculator.sumAllEven(numbers, (number) -> number % 2 == 0);
         assertThat(sum).isEqualTo(12);
     }
 
     @Test
     public void sumAllOverThree() {
-        int sum = Calculator.sumAllOverThree(numbers);
+        int sum = Calculator.sumAllOverThree(numbers, (number) -> number > 3);
         assertThat(sum).isEqualTo(15);
+    }
+
+    @Test
+    public void sumByConditional() {
+        int sumAll = Calculator.sumByConditional(numbers, (number) -> true);
+        int sumAllEven = Calculator.sumByConditional(numbers, (number) -> number % 2 ==0);
+        int sumAllOverThree = Calculator.sumByConditional(numbers, (number) -> number > 3);
+
+        assertThat(sumAll).isEqualTo(21);
+        assertThat(sumAllEven).isEqualTo(12);
+        assertThat(sumAllOverThree).isEqualTo(15);
     }
 }
