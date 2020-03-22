d4a823f0e98f578733526504ded1600ae60dcf67
giantim <giantim132@gmail.com>
2020-03-22 11:30:43 +0900

    feat: stream 을 이용하여 단어 정렬 기능 구현

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -6,7 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamStudy {
     public static long countWords() throws IOException {
@@ -22,26 +24,24 @@ public class StreamStudy {
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
-        for(Integer number: numbers) {
-            result.add(2 * number);
-        }
-
-        return result;
+        return numbers.stream()
+                .map(number -> number * 2)
+                .collect(Collectors.toList());
     }
 
     public static long sumAll(List<Integer> numbers) {
-        int result = 0;
-
-        for(Integer number: numbers) {
-            result += number;
-        }
-
-        return result;
+//        return numbers.stream()
+//                .mapToInt(Integer::intValue)
+//                .sum();
+        return numbers.stream()
+                .reduce(0, (n1, n2) -> n1 = n1 + n2);
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return numbers.stream().filter(v -> v > 3).mapToInt(v -> v * 2).sum();
+        return numbers.stream()
+                .filter(v -> v > 3)
+                .mapToInt(v -> v * 2)
+                .sum();
     }
 
     public static void printLongestWordTop100() throws IOException {
@@ -49,8 +49,15 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        System.out.println(words);
-        System.out.println(words.size());
+//        System.out.println(words);
+//        System.out.println(words.size());
         // TODO 이 부분에 구현한다.
+        words.stream()
+                .filter(word -> word.length() > 12)
+                .map(String::toLowerCase)
+                .sorted(Comparator.comparing(String::length))
+                .distinct()
+                .limit(100)
+                .forEach(System.out::println);
     }
 }
